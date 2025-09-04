12df76ed08b2047ad9b270e4b3f4cc0e8c27a5fe
0xar-ds <sadden-rope-sleek@duck.com>
2025-09-03 23:03:57 -0300

    style: format files

--- tests/moon.test.ts
+++ tests/moon.test.ts
@@ -5,17 +5,17 @@ import { expect, test } from "vitest";
 const indexJs = path.resolve("dist/index.js");
 
 test("basic", async () => {
-  const cwd = path.join(import.meta.dirname, "workspaces/basic");
-  const { stdout, stderr } = await $({ cwd })`node ${indexJs}`;
-  expect(stdout).toMatchSnapshot();
-  expect(stderr).toBe("");
+	const cwd = path.join(import.meta.dirname, "workspaces/basic");
+	const { stdout, stderr } = await $({ cwd })`node ${indexJs}`;
+	expect(stdout).toMatchSnapshot();
+	expect(stderr).toBe("");
 });
 
 test("no-tasks", async () => {
-  const cwd = path.join(import.meta.dirname, "workspaces/no-tasks");
+	const cwd = path.join(import.meta.dirname, "workspaces/no-tasks");
 
-  const { stdout, stderr } = await $({ cwd })`node ${indexJs}`;
+	const { stdout, stderr } = await $({ cwd })`node ${indexJs}`;
 
-  expect(stdout).toMatchSnapshot();
-  expect(stderr).toBe("");
+	expect(stdout).toMatchSnapshot();
+	expect(stderr).toBe("");
 });
